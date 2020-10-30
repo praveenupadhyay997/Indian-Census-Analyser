79e85fd2e08fb2ae80134bc828bbdb02e44c592a
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-30 17:53:59 +0530

    [Praveen] Add: Implemented the test case to pass the wrong state code file path and see whether the returned exception matches.

--- IndianCensusDataClass/NUnitTestCensusAnalyser/UnitTestClass.cs
+++ IndianCensusDataClass/NUnitTestCensusAnalyser/UnitTestClass.cs
@@ -19,6 +19,9 @@ namespace NUnitTestCensusAnalyser
         public static string wrongIndianStateCensusFileType = @"F:\Program files(x64)\Microsoft Visual Studio\BridgeLabzAssignments\Indian-Census-Analyser\IndianCensusDataClass\IndianCensusDataClass\CSVFiles\IndiaStateCensusData.txt";
         public static string wrongDelimeterIndianStateCensusFilePath = @"F:\Program files(x64)\Microsoft Visual Studio\BridgeLabzAssignments\Indian-Census-Analyser\IndianCensusDataClass\IndianCensusDataClass\CSVFiles\DelimiterIndiaStateCensusData.csv";
         public static string wrongHeaderIndianStateCensusFilePath = @"F:\Program files(x64)\Microsoft Visual Studio\BridgeLabzAssignments\Indian-Census-Analyser\IndianCensusDataClass\IndianCensusDataClass\CSVFiles\WrongIndiaStateCensusData.csv";
+        
+        public static string wrongIndianStateCodeFilePath = @"C:\Program files(x64)\Microsoft Visual Studio\BridgeLabzAssignments\Indian-Census-Analyser\IndianCensusDataClass\IndianCensusDataClass\CSVFiles\IndiaStateCode.csv";
+
         CensusAnalyser censusAnalyser;
         Dictionary<string, CensusDTO> totalRecord;
         Dictionary<string, CensusDTO> stateRecord;
@@ -68,6 +71,25 @@ namespace NUnitTestCensusAnalyser
         {
             var indianStateCensusResult = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, wrongHeaderIndianStateCensusFilePath, indianStateCensusHeaders));
             Assert.AreEqual(CensusAnalyserException.Exception.INCORRECT_HEADER, indianStateCensusResult.exception);
+        }
+        /// <summary>
+        /// TC 2.1 - To get the records and to assert whether the count of all the records matches to manually addressed or not
+        /// Using the Dictionary Collection to store the Indian State Census Records and then Count it
+        /// </summary>
+        [Test]
+        public void GivenIndianStateCodeData_ReturnsCorrectCount()
+        {
+            stateRecord = censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, indianStateCodeFilePath, indianStateCodeHeaders);
+            Assert.AreEqual(37, stateRecord.Count);
+        }
+        /// <summary>
+        /// TC 2.2 - To pass a wrong file path and assert whether the custom exception of file not found is returned or not
+        /// </summary>
+        [Test]
+        public void GivenWrongStateCodeFile_ShouldReturnCustomException()
+        {
+            var indianStateCensusResult = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, wrongIndianStateCodeFilePath, indianStateCodeHeaders));
+            Assert.AreEqual(CensusAnalyserException.Exception.FILE_NOT_FOUND, indianStateCensusResult.exception);
         }
     }
 }
