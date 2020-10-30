68a8cb99098a6bd656f1b6fa305e7cb2a21024d4
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-30 17:57:15 +0530

    [Praveen] Add: Implemented the test case to pass the wrong file type should return the custom exception and then assert it.

--- IndianCensusDataClass/NUnitTestCensusAnalyser/UnitTestClass.cs
+++ IndianCensusDataClass/NUnitTestCensusAnalyser/UnitTestClass.cs
@@ -21,6 +21,7 @@ namespace NUnitTestCensusAnalyser
         public static string wrongHeaderIndianStateCensusFilePath = @"F:\Program files(x64)\Microsoft Visual Studio\BridgeLabzAssignments\Indian-Census-Analyser\IndianCensusDataClass\IndianCensusDataClass\CSVFiles\WrongIndiaStateCensusData.csv";
         
         public static string wrongIndianStateCodeFilePath = @"C:\Program files(x64)\Microsoft Visual Studio\BridgeLabzAssignments\Indian-Census-Analyser\IndianCensusDataClass\IndianCensusDataClass\CSVFiles\IndiaStateCode.csv";
+        public static string wrongIndianStateCodeFileType = @"F:\Program files(x64)\Microsoft Visual Studio\BridgeLabzAssignments\Indian-Census-Analyser\IndianCensusDataClass\IndianCensusDataClass\CSVFiles\IndiaStateCode.txt";
 
         CensusAnalyser censusAnalyser;
         Dictionary<string, CensusDTO> totalRecord;
@@ -90,6 +91,15 @@ namespace NUnitTestCensusAnalyser
         {
             var indianStateCensusResult = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, wrongIndianStateCodeFilePath, indianStateCodeHeaders));
             Assert.AreEqual(CensusAnalyserException.Exception.FILE_NOT_FOUND, indianStateCensusResult.exception);
+        }
+        /// <summary>
+        /// TC 2.3 - To pass a wrong file type for state code and the correct file name and assert whether the custom exception of file not found is returned or not
+        /// </summary>
+        [Test]
+        public void GivenWrongStateCodeFileType_ShouldReturnCustomException()
+        {
+            var indianStateCensusResult = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, wrongIndianStateCodeFileType, indianStateCodeHeaders));
+            Assert.AreEqual(CensusAnalyserException.Exception.INVALID_FILE_TYPE, indianStateCensusResult.exception);
         }
     }
 }
